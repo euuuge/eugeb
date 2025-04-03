64c955653baadedf33cd8b072b4ba9c2aa9bd48d
euuuge <eugeniabarreto@uca.edu.ar>
2025-04-03 19:10:44 -0300

    Add files via upload

---
+++ eb5.py
@@ -0,0 +1,12 @@
+#while #repite un bloque mientras la condicion sea verdadera
+# contador = 3 //asigno un valor a la variable
+# contador == 5 // hacer comparacion 
+contador=0
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
+
+   
+  
